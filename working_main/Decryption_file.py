028c993e18d3c63124f26a676c3673c0e6ae62df
piyush kheria <piyushkheria23@gmail.com>
2025-04-28 19:26:35 +0530

    "completed my part"

---
+++ working_main/Decryption_file.py
@@ -0,0 +1,52 @@
+from PIL import Image ,ImageDraw
+from common import binary_to_message, decode_number , generate_starting_points , hex_to_binary
+from main_decrypt import dfs_decryption
+from config import get_depth_limit, get_height, get_width, get_password
+import random
+
+def get_info(binary_message):
+    return_lst = []
+    for i in range(0,6):
+        return_lst.append(decode_number(binary_message[i*16:(i+1)*16]))
+    return_lst.append(binary_to_message(binary_message[96:176]))
+    return return_lst
+
+
+def decrypting(starting_x, starting_y):
+    start_x, start_y = starting_x,starting_y
+    pixels = img.load()
+    visited = set()
+    for i in start_points:
+        visited.add(i)
+    path = []
+    final_message=""
+    i=0
+    
+    while True:
+        message_extraction = ""
+        message_extraction = dfs_decryption(start_x, start_y,False, pixels, visited, path, message_extraction)
+        next_x = decode_number(message_extraction[0:16])
+        next_y = decode_number(message_extraction[16:32])
+        donne , check = binary_to_message(message_extraction[32:])
+        final_message += donne
+        start_x, start_y = next_x , next_y
+        if check :
+            break
+        i+=1
+
+    return final_message
+
+
+if __name__ == "__main__":
+    img = Image.open("graph_based_hashing.png").convert("RGBA")
+    width, height = img.size
+    get_width(width)
+    get_height(height)
+    entry = input("Enter the password :\n")
+    info = get_info(hex_to_binary(entry))
+    check_point = get_password(info[6][0].rstrip('@'))
+    get_depth_limit((int)(info[2]))
+    start_points = generate_starting_points(width,height,(int)(info[5]),50)
+    random.shuffle(start_points)
+    answer = decrypting((int)(info[3]) , (int)(info[4]))
+    print(answer)
---
+++ working_main/Encryption_file.py
@@ -0,0 +1,55 @@
+from PIL import Image ,ImageDraw
+from common import message_to_binary,encode_number ,generate_starting_points , binary_to_hex
+from main_encrypt import dfs_encryption
+from config import get_depth_limit, get_height, get_width, get_password
+import random
+
+def encrptying(start_points):
+    pixels = img.load()
+    message = get_message()
+    print("Message Length",len(message))
+    visited = set()
+    for coordinate in start_points:
+        visited.add(coordinate)
+    path = []
+    enc_msg = message_to_binary(message)
+    i =0
+    data_index = 0
+    while True:
+        start_x, start_y = start_points[i]
+        next_start_x, next_start_y = start_points[i+1]
+        next_point_binary = encode_number(next_start_x) + encode_number(next_start_y)
+        enc_msg = enc_msg[:data_index]+next_point_binary+enc_msg[data_index:]
+        data_index = dfs_encryption(start_x, start_y,False,enc_msg, pixels, visited, path, data_index)
+        if data_index >= len(enc_msg):
+            break
+        i+=1
+
+    img.save("graph_based_hashing.png")
+
+def get_message():
+    return input("Enter your Secret Message :\n")
+
+if __name__ == "__main__":
+    img = Image.open("/Users/piyushkheria/Desktop/steganography/test.png").convert("RGBA")
+    width, height = img.size
+    get_width(width)
+    get_height(height)
+    word = input("Enter a word (must not be more than 10 letters):\n")
+    check_point = get_password(word)
+    print(word)
+    while len(word) <15:
+        word += "@"
+    if len(word) >15:
+        word = word[:15]
+
+    prime = int(input("choose one 19 or 23 :\n"))
+    get_depth_limit(prime)
+    randomise_it = int(input("toggle value (enter a value less than 2000): \n"))
+    start_points = generate_starting_points(width,height,randomise_it,50)
+    start = start_points[0]
+
+    My_password = encode_number(width) + encode_number(height)+ encode_number(prime)+ encode_number(start[0]) + encode_number(start[1])+ encode_number(randomise_it)+ message_to_binary(word)+"11111111111111111111111111111111"
+
+    encrptying(start_points)
+    print("Your Password :",binary_to_hex(My_password))
